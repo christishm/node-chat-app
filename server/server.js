ff511c3a2a8c9d826b7acba0bcefc015ee705638
christish <christishm@gmail.com>
2018-04-02 18:07:25 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname,'../public');
-//const publicPath = __dirname + '..'+'/public';
+const port = process.env.PORT || 3000;
 var app = express();
-//app.set(express.static(publicPath));
 app.use(express.static(publicPath));
 console.log('Path is ....'+publicPath);
-app.listen(3001);
+app.listen(port,()=>{
+  console.log(`Listening to the port ${port}`);
+});
