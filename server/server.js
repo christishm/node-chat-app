9281a22e9602f2cd1c95900d83627415353f1066
christish <christishm@gmail.com>
2018-04-02 16:55:14 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname,'../public');
+var app = express();
+app.set(express.static(publicPath));
+console.log('Path is ....'+publicPath);
+app.listen(3000);
