d1d6e653f58bf300bf6377de90381bd8e4891d7d
christish <christishm@gmail.com>
2018-04-02 17:58:38 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname,'../public');
+//const publicPath = __dirname + '..'+'/public';
 var app = express();
-app.set(express.static(publicPath));
+//app.set(express.static(publicPath));
+app.use(express.static(publicPath));
 console.log('Path is ....'+publicPath);
 app.listen(3001);
