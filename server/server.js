814372f7591ec331136372b0bd8aa8dd75d0d82f
christish <christishm@gmail.com>
2018-04-02 17:14:32 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -4,4 +4,4 @@ const publicPath = path.join(__dirname,'../public');
 var app = express();
 app.set(express.static(publicPath));
 console.log('Path is ....'+publicPath);
-app.listen(3000);
+app.listen(3001);
